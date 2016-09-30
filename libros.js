0fd7acd4247371bb5db78a51e788f820e0b9ce88
Alejandro Turriza <giezidine_01@hotmail.com>
2016-09-30 01:49:42 -0500

    actualizacion de archivos corrector

--- libros.js
+++ libros.js
@@ -1,12 +1,36 @@
-var app = angular.module('app', [ngResource]);
+var app = angular.module('app', []);
 
-app.factory("Post", function($resource){
-	return $resource("http://127.0.0.1:5000/libros/:id");
-});
-
-app.controller("Libros", function($scope, $http){
-	$http.get('http://127.0.0.1:5000/libros').
+app.controller('Libros', function($scope, $http){
+	$http.get('http://localhost:5000/libros/').
 	then(function(response){
 		$scope.libros = response.data;
 	});
 });
+
+app.controller('AgregaLibro', function($scope, $http){
+	$scope.EnviarDatos = function(){
+		var data = $.param({
+			titulo: $scope.titulo,
+			descripcion: $scope.descripcion
+		});
+
+		var config = {
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+			}
+		}
+
+		$http.post('http://localhost:5000/libros/', data, config)
+		.success(function(data, status, header, config){
+			$scope.PostDataResponse = data;
+		})
+		.error(function(data, status, header, config){
+			$scope.ResponseDetails = "Data: " + data +
+                    "<hr />status: " + status +
+                    "<hr />headers: " + header +
+                    "<hr />config: " + config;
+		});
+	};
+});
+
+
