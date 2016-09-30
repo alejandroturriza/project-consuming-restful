c3c41a0586c8ba75ed5b0b013bc9ec69571857ed
Alejandro Turriza <giezidine_01@hotmail.com>
2016-09-30 03:10:22 -0500

    recuperacion de datos
    - modificación del añadido de libro para que se actualice la table
    - ver datos de un libro

--- libros.js
+++ libros.js
@@ -1,13 +1,17 @@
 var app = angular.module('app', []);
 
-app.controller('Libros', function($scope, $http){
+app.factory('servicioDatos', function(){
+	libros: {}
+});
+
+app.controller('Libros', function($scope, $http, $rootScope){
 	$http.get('http://localhost:5000/libros/').
 	then(function(response){
-		$scope.libros = response.data;
+		$rootScope.libros = response.data;
 	});
 });
 
-app.controller('AgregaLibro', function($scope, $http){
+app.controller('AgregaLibro', function($scope, $http, $rootScope){
 	$scope.EnviarDatos = function(){
 		var data = $.param({
 			titulo: $scope.titulo,
@@ -23,12 +27,25 @@ app.controller('AgregaLibro', function($scope, $http){
 		$http.post('http://localhost:5000/libros/', data, config)
 		.success(function(data, status, header, config){
 			$scope.PostDataResponse = data;
+			$http.get('http://localhost:5000/libros/').
+			then(function(response){
+				$rootScope.libros = response.data;
+			});
 		})
 		.error(function(data, status, header, config){
 			$scope.ResponseDetails = "Data: " + data +
                     "<hr />status: " + status +
                     "<hr />headers: " + header +
                     "<hr />config: " + config;
+		});
+	};
+});
+
+app.controller('Libro', function($scope, $http, $rootScope){
+	$scope.VerLibro = function(id){
+		$http.get('http://localhost:5000/libros/' + id).
+		then(function(response){
+			$rootScope.libros = response.data;
 		});
 	};
 });
